3874445ad2e1db601790692b9632725ac11efe5b
Yopiter <Martinius.schneider@web.de>
2017-11-27 10:09:25 +0100

    Fibonacci repariert und vervollständigt

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,13 +1,19 @@
 #! /usr/bin/python3
 
 def iterativ(n):
+ if(n<=0):
+  return 0
+ elif(n==1):
+  return 1
  i0=0
  i1=1
- for counter in range(n-2):
+ i=0
+ for counter in range(n-1):
   i=i0+i1
   i0=i1
   i1=i
  return i
 
 n=int(input("Anzahl? "))
-print(iterativ(n))
+for l in range(n+1):
+ print("F("+str(l)+"):",iterativ(l))
